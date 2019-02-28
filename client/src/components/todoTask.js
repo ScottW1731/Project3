5e4fba3039d933700e9a65a16f9dbbc73c26df41
Byron <byron.p.roberts@gmail.com>
2019-02-28 11:54:20 -0600

    broken up taskpage

---
+++ client/src/components/todoTask.js
@@ -0,0 +1,88 @@
+class TodoTask extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        isEdit: false,
+        isComplete: false
+      };
+    }
+  
+    deleteTask = () => {
+      const { id, deleteTask } = this.props;
+      deleteTask(id);
+    };
+  
+    editTask = () => {
+      this.setState({
+        isEdit: true
+      });
+    };
+  
+    completeTask = () => {
+      const { completeTask, id } = this.props;
+      completeTask(id);
+      this.setState({
+        isComplete: true
+      });
+    };
+  
+    saveEditTask = e => {
+      e.preventDefault();
+  
+      const { id } = this.props;
+      const { saveEditTask } = this.props;
+  
+      if (!this.editInput.value) {
+        null;
+      } else {
+        saveEditTask(this.editInput.value, id);
+        this.setState({
+          isEdit: false
+        });
+      }
+    };
+  
+    render() {
+      const { task, type } = this.props;
+      const { isEdit, isComplete } = this.state;
+      const disableBtn = isComplete && "disable-btn";
+  
+      return (
+        <li>
+          {isEdit ? (
+            <form id="edit-task-form" onSubmit={this.saveEditTask}>
+              <input
+                defaultValue={task}
+                ref={editInput => (this.editInput = editInput)}
+              />
+              <button>Save</button>
+            </form>
+          ) : (
+            <div>
+              <p className={isComplete && "completed"}>
+                {task} <span>{type}</span>
+              </p>
+              <button className="delete-btn" onClick={this.deleteTask}>
+                Delete
+              </button>
+              <button
+                className={disableBtn}
+                disabled={isComplete}
+                onClick={this.editTask}
+              >
+                Edit
+              </button>
+              <button
+                className={disableBtn}
+                disabled={isComplete}
+                onClick={this.completeTask}
+              >
+                Complete
+              </button>
+            </div>
+          )}
+        </li>
+      );
+    }
+  }
+  
