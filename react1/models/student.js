4955cf081c26523986de4fc1737269f464c6c674
Scott Wagner <af17317596@gmail.com>
2019-02-28 21:29:20 -0600

    added a seedDB

--- react1/models/student.js
+++ react1/models/student.js
@@ -4,10 +4,11 @@ const Schema = mongoose.Schema;
 const studentSchema = new Schema({
   studentname: { type: String, required: true },
   email: { type: String, required: true },
-//   mentorname: String,
+
   date: { type: Date, default: Date.now }
 });
 
+//   mentorname: String,
 const student = mongoose.model("student", studentSchema);
 
 module.exports = student;
---
+++ react1/scripts/seedDB.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose");
+const db = require("../models");
+
+// This file empties the students collection and inserts the students below
+
+mongoose.connect(
+  process.env.MONGODB_URI ||
+  "mongodb://localhost/reactreadinglist"
+);
+
+const studentSeed = [
+  {
+    title : "student", 
+    first : "Scott", 
+    last : "Wagner", 
+    email : "example1@gmail.com", 
+    date: new Date(Date.now())
+  },
+  {
+    title : "student", 
+    first : "Homer", 
+    last : "Simpson", 
+    email : "example2@gmail.com"
+  },
+  {
+    title : "student", 
+    first : "Bart", 
+    last : "Simpson", 
+    email : "example3@gmail.com"
+  },
+  {
+    title : "student", 
+    first : "Marge", 
+    last : "Simpson", 
+    email : "example4@gmail.com"
+  },
+  {
+    title : "student", 
+    first : "Lisa", 
+    last : "Simpson", 
+    email : "example5@gmail.com"
+  },
+  {
+    title : "student", 
+    first : "Maggie", 
+    last : "Simpson", 
+    email : "example2@gmail.com", 
+  }
+];
+
+db.student
+  .remove({})
+  .then(() => db.student.collection.insertMany(studentSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
