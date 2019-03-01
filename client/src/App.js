8a2faadc9f68bb528170501d91ee56118476abfd
Scott Wagner <af17317596@gmail.com>
2019-03-01 11:38:53 -0600

    fixed syntax errors in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,18 @@
-
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Task from '../src/pages/TaskPage';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <Task/>
-
+    return ( 
+    <div className = "App" >
+      <Task /> 
       </div>
-    </Router>
-  );
+    );
+  };
 }
 
 export default App;
