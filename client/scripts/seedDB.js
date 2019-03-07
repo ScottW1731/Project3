40fdaebbe5e05410348318f2eeba5b284881839f
Scott Wagner <af17317596@gmail.com>
2019-03-07 10:44:43 -0600

    added email section

--- client/scripts/seedDB.js
+++ client/scripts/seedDB.js
@@ -15,37 +15,43 @@ const studentSeed = [
     first : "Scott", 
     last : "Wagner", 
     password : "example11", 
+    email:"example11@gmail.com",
     date: new Date(Date.now())
   },
   {
     title : "student", 
     first : "Homer", 
     last : "Simpson", 
-    password : "example21"
+    password : "example21",
+    email:"example21@gmail.com"
   },
   {
     title : "student", 
     first : "Bart", 
     last : "Simpson", 
-    password : "example31"
+    password : "example31",
+    email:"example31@gmail.com"
   },
   {
     title : "student", 
     first : "Marge", 
     last : "Simpson", 
-    password : "example41"
+    password : "example41",
+    email:"example41@gmail.com"
   },
   {
     title : "student", 
     first : "Lisa", 
     last : "Simpson", 
-    password : "example51"
+    password : "example51",
+    email:"example51@gmail.com"
   },
   {
     title : "student", 
     first : "Maggie", 
     last : "Simpson", 
     password : "example61", 
+    email:"example61@gmail.com"
   }
 ];
 
@@ -67,37 +73,43 @@ const mentorSeed = [
     first : "Jamie", 
     last : "Benn", 
     password : "example10", 
+    email:"example10@gmail.com",
     date: new Date(Date.now())
   },
   {
     title : "mentor", 
     first : "Tyler", 
     last : "Seguin", 
-    password : "example20"
+    password : "example20",
+    email:"example20@gmail.com"
   },
   {
     title : "mentor", 
     first : "Alexander", 
     last : "Radulov", 
-    password : "example30"
+    password : "example30",
+    email:"example30@gmail.com"
   },
   {
     title : "mentor", 
     first : "Radek", 
     last : "Faksa", 
-    password : "example40"
+    password : "example40",
+    email:"example40@gmail.com"
   },
   {
     title : "mentor", 
     first : "Ben", 
     last : "Bishop", 
-    password : "example50"
+    password : "example50",
+    email:"example50@gmail.com"
   },
   {
     title : "mentor", 
     first : "Anton", 
     last : "Khudobin", 
-    password : "example60", 
+    password : "example60",
+    email:"example60@gmail.com"
   }
 ];
 
