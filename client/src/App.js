1aa3b66a5903548663bcac628938b9020a9db1b8
Byron <byron.p.roberts@gmail.com>
2019-03-09 11:35:44 -0600

    route changes on APP.js added pkg.json elems and About page.. WIP

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import LandingPage from "./pages/LandingPage";
 import Admin from "./pages/Admin/Admin";
 import TaskPage from "./pages/TaskPage";
 import Mentor from "./pages/Mentor";
+import About from "./pages/About";
 // import Detail from "./pages/Detail";
 // import NoMatch from "./pages/NoMatch";
 
@@ -14,10 +15,11 @@ function App() {
       <div>
         <Switch>
           <Route exact path="/" component={LandingPage} />
-          <Route exact path="/landingpage" component={LandingPage} />
-          <Route exact path="/admin" component={Admin} />
+          {/* <Route exact path="/landingpage" component={LandingPage} /> */}
+          <Route exact path="/Admin0" component={Admin} />
           <Route exact path="/mentor" component={Mentor} />
           <Route exact path="/task" component={TaskPage} />
+          <Route exact path="/Admin2" component={About}/>
           {/* <Route exact path="/task/:id" component={Detail} />
           <Route component={NoMatch} /> */}
         </Switch>
