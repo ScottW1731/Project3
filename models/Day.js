976e799430c23bcd93b7004426f1d0b999783a75
Byron <byron.p.roberts@gmail.com>
2019-03-13 22:11:57 -0500

    fixed Day.js

--- models/Day.js
+++ models/Day.js
@@ -2,19 +2,22 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const daySchema = new Schema({
-    student: {
-        type: Schema.Types.ObjectId,
-        ref: "Student"
+    // student: {
+    //     type: Schema.Types.ObjectId,
+    //     ref: "Student"
+    // },
+    Date: {
+        type: Date
     },
-    percentageComplete: {
+    value: {
         type: Number
     },
-    mood: {
-        type: String
-    },
-    comment: {
-        type: String
-    }
+    // mood: {
+    //     type: String
+    // },
+    // comment: {
+    //     type: String
+    // }
 })
 
 const Day = mongoose.model("Day", daySchema);
